cb97293710980c03585992c226cd09cf58a0911c
jj <jj>
2021-03-03 00:59:33 +0100

    gatsby-v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,6 +114,9 @@ module.exports = {
                 defer: false,
             },
         },
+        {
+            resolve: `gatsby-plugin-gatsby-cloud`,
+        },          
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // This plugin is currently causing issues: https://github.com/gatsbyjs/gatsby/issues/25360
         //`gatsby-plugin-offline`,
